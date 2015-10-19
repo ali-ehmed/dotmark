7c5008e23b0443eb844c48747da30a677df2f6d2
Ali Ahmed <ali_ahmed922@outlook.com>
2015-10-20 01:02:49 +0500

    application setup done

---
+++ app/admin/create_admin_service.rb
@@ -0,0 +1,9 @@
+class CreateAdminService
+  def call
+    admin = Admin.find_or_create_by!(email: Rails.application.secrets.admin_email) do |admin|
+      admin.password = Rails.application.secrets.admin_password
+      admin.password_confirmation = Rails.application.secrets.admin_password
+      # admin.confirm!
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  devise_for :admins
+  
+  authenticated :admin do
+    root 'landing#dashboard', as: :authenticated_root
+  end
+
+  unauthenticated :admin do
+    devise_scope :admin do 
+      get "/", to: "devise/sessions#new"
+    end
+  end
+  
 end
