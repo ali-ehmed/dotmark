f61637adddd715bddd7c42c3e60c7bd467d3db6f
Ali Ahmed <ali_ahmed922@outlook.com>
2016-01-06 19:52:59 +0500

    minor changed

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ApplicationMailer
+	default template_path: 'notification_mailer'
 	def send_allocation_instructions(teacher, allocations)
 		@teacher = teacher
 		@allocations = allocations
+		@teacher_dashboard_url = teachers_login_url(resource: @teacher.username, subdomain: "#{@teacher.account.subdomain}")
 		mail(to: @teacher.email, subject: 'Allocation Instructions')
 	end
 end
